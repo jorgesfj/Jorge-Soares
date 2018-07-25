f54e8b7ec7089d7f8aa75f6b224b09da1c358e2a
Jorge <junior-soares42@hotmail.com>
2018-07-25 17:19:03 -0300

    Consertando romanos

--- Atividade-Condicionais_e_Estruturas_de_Repetição/romanos.py
+++ Atividade-Condicionais_e_Estruturas_de_Repetição/romanos.py
@@ -1,39 +1,69 @@
 romano = input()
 lista = []
 lista_int = []
+lista.append('a')
 for i in range(len(romano)):
 	lista.append(romano[i])
 for i in range(len(lista)):
 	if lista[i] == 'I':
-		lista_int.append(int('1'))
-	if lista[i] == 'V':
-		lista_int.append(int('5'))
-	if lista[i] == 'X':
-		lista_int.append(int('10'))
-	if lista[i] == 'L':
-		lista_int.append(int('50'))
-	if lista[i] == 'C':
-		lista_int.append(int('100'))
-	if lista[i] == 'D':
-		lista_int.append(int('500'))
-	if lista[i] == 'M':
+		if i+1 <len(lista):
+			if lista[i+1] == 'V':
+				lista_int.append(int('-1'))
+			elif lista[i+1] == 'V':
+				lista_int.append(int('-1'))
+			else:
+				lista_int.append(int('1'))
+		else:
+			lista_int.append(int('1'))
+	elif lista[i] == 'V':
+		if i+1 <len(lista):
+			if lista[i+1] == 'X':
+				lista_int.append(int('-5'))
+			else:
+				lista_int.append(int('5'))
+		else:		
+			lista_int.append(int('5'))
+	elif lista[i] == 'X':
+		if i+1 <len(lista):
+			if lista[i+1] == 'L':
+				lista_int.append(int('-10'))
+			else:
+				lista_int.append(int('10'))
+		else:
+			lista_int.append(int('10'))
+	elif lista[i] == 'L':
+		if i+1 <len(lista):
+			if lista[i+1] == 'C':
+				lista_int.append(int('-50'))
+			else:
+				lista_int.append(int('50'))
+		else:
+			lista_int.append(int('50'))
+	elif lista[i] == 'C':
+		if i+1 <len(lista):
+			if lista[i+1] == 'D':
+				lista_int.append(int('-100'))
+			else:
+				lista_int.append(int('100'))
+		else:
+			lista_int.append(int('100'))
+	elif lista[i] == 'D':
+		if i+1 <len(lista):
+			if lista[i+1] == 'M':
+				lista_int.append(int('-500'))
+			else:
+				lista_int.append(int('500'))
+		else:
+			lista_int.append(int('500'))
+	elif lista[i] == 'M':
 		lista_int.append(int('1000'))
-
-if len(lista_int)>=2:
-	if lista_int[0]>=lista_int[1]:
-			subtra = sum(lista_int)
-	else:
-		lista_int.sort(key=int, reverse=True)
-		subtra = lista_int[0]
-		for i in range(1,len(lista_int)):
-			subtra = subtra - lista_int[i]
-else:
-	subtra = lista_int[0]
+soma = 0
+for i in range(len(lista_int)):
+	soma += lista_int[i]
 
 
-
-if subtra%5 == 0:
+if soma%5 == 0:
 	print("O numero é multiplo de 5")
 else:
-	var = subtra%5
+	var = soma%5
 	print("O resto pela divisao por 5 do numero dado e igual a {}!".format(var))
