15c0c379b4b103bd154af1cfff8853882f276ae4
Jorge <junior-soares42@hotmail.com>
2018-07-27 12:51:57 -0300

    Otimizando romanos

--- Atividade-Condicionais_e_Estruturas_de_Repetição/romanos.py
+++ Atividade-Condicionais_e_Estruturas_de_Repetição/romanos.py
@@ -1,69 +1,32 @@
-romano = input()
-lista = []
-lista_int = []
-lista.append('a')
-for i in range(len(romano)):
-	lista.append(romano[i])
-for i in range(len(lista)):
-	if lista[i] == 'I':
-		if i+1 <len(lista):
-			if lista[i+1] == 'V':
-				lista_int.append(int('-1'))
-			elif lista[i+1] == 'V':
-				lista_int.append(int('-1'))
-			else:
-				lista_int.append(int('1'))
-		else:
-			lista_int.append(int('1'))
-	elif lista[i] == 'V':
-		if i+1 <len(lista):
-			if lista[i+1] == 'X':
-				lista_int.append(int('-5'))
-			else:
-				lista_int.append(int('5'))
-		else:		
-			lista_int.append(int('5'))
-	elif lista[i] == 'X':
-		if i+1 <len(lista):
-			if lista[i+1] == 'L':
-				lista_int.append(int('-10'))
-			else:
-				lista_int.append(int('10'))
-		else:
-			lista_int.append(int('10'))
-	elif lista[i] == 'L':
-		if i+1 <len(lista):
-			if lista[i+1] == 'C':
-				lista_int.append(int('-50'))
-			else:
-				lista_int.append(int('50'))
-		else:
-			lista_int.append(int('50'))
-	elif lista[i] == 'C':
-		if i+1 <len(lista):
-			if lista[i+1] == 'D':
-				lista_int.append(int('-100'))
-			else:
-				lista_int.append(int('100'))
-		else:
-			lista_int.append(int('100'))
-	elif lista[i] == 'D':
-		if i+1 <len(lista):
-			if lista[i+1] == 'M':
-				lista_int.append(int('-500'))
-			else:
-				lista_int.append(int('500'))
-		else:
-			lista_int.append(int('500'))
-	elif lista[i] == 'M':
-		lista_int.append(int('1000'))
-soma = 0
-for i in range(len(lista_int)):
-	soma += lista_int[i]
+n = input()
+arabicos = []
+nums = {'M':1000,
 
+'D':500,
 
-if soma%5 == 0:
+'C':100,
+
+'L':50,
+
+'X':10,
+
+'V':5,
+
+'I':1}
+
+for i in range(len(n)):
+	if n[i] in nums:
+		arabicos.append(int(nums[n[i]]))
+
+suma = int(arabicos[len(arabicos)-1])
+for i in range(len(arabicos)-2,-1,-1):
+	if arabicos[i]<suma:
+		suma -= arabicos[i]
+	if arabicos[i]>=suma:
+		suma += arabicos[i]
+
+if suma%5 == 0:
 	print("O numero é multiplo de 5")
 else:
-	var = soma%5
+	var = suma%5
 	print("O resto pela divisao por 5 do numero dado e igual a {}!".format(var))
