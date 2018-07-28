33bef7ddc06a24e833821029bfce2479278aa03e
Jorge <junior-soares42@hotmail.com>
2018-07-27 21:46:11 -0300

    consertando arrays

--- Atividade-Condicionais_e_Estruturas_de_Repetição/brincando_Com_Arrays.py
+++ Atividade-Condicionais_e_Estruturas_de_Repetição/brincando_Com_Arrays.py
@@ -1,15 +1,22 @@
 quantidade = int(input())
+
 numeros = input()
-print(numeros[::-1])
+
+print(numeros[::-1],'\n')
 lista3 = []
 
 lista = numeros.split()
-lista3.append(lista[1:len(lista)])
+for i in range(1,len(lista)):
+	lista3.append(lista[i])
 lista3.append(lista[0])
+for i in range(len(lista3)):
+	print(lista3[i],'', end = '')
+print('\n')
 
-print(lista3)
 lista2 = []
 for i in range(len(lista)):
 	lista2.append(int(lista[i]))
 
-print(sorted(lista2, key=int, reverse=True))
+final = sorted(lista2, key=int, reverse=True)
+for i in range(len(final)):
+	print(final[i],'', end='')
