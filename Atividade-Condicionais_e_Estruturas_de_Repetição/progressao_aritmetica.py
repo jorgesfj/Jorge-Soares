879f47e04e1714b35ff6cf89f3824c8f16f45dc6
Jorge <junior-soares42@hotmail.com>
2018-08-03 21:26:42 -0300

    conserto progressao

--- Atividade-Condicionais_e_Estruturas_de_Repetição/progressao_aritmetica.py
+++ Atividade-Condicionais_e_Estruturas_de_Repetição/progressao_aritmetica.py
@@ -1,7 +1,9 @@
 n = int(input())
 elementos = input()
 lista = []
+lista_auxiliar2 = []
 dicionario = {}
+cont = 0
 lista_elementos = []
 lista_aux = elementos.split()
 for i in range(len(lista_aux)):
@@ -10,7 +12,10 @@ for i in range(0,n):
 	if i+1 < n:
 		razao = lista_elementos[i] - lista_elementos[i+1]
 		dicionario[razao] = razao
+		lista_auxiliar2.append(razao)
 	else:
 		break
-
+for i in range (len(lista_auxiliar2)):
+		if lista_auxiliar2.count(lista_auxiliar2[i]) > 1 :
+			cont += 1
 print(len(dicionario))
