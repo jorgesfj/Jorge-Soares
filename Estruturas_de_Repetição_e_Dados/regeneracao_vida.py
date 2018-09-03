944114bff44c6faf9f99a0fc8d8fe8f15ed90964
Jorge <junior-soares42@hotmail.com>
2018-09-03 19:19:08 -0300

    add

--- Estruturas_de_Repetição_e_Dados/regeneracao_vida.py
+++ Estruturas_de_Repetição_e_Dados/regeneracao_vida.py
@@ -1,12 +1,15 @@
 vida = 100
 dano_total = 0
+dano_total_lista = []
 tempo = []
 danos = int(input())
 cont = 0
-while cont<danos:
+while cont<danos and aux<100:
 	cont+=1
 	x,y = list(map(int,input().split()))
+	aux = x
 	dano_total += x
+	dano_total_lista.append(x)
 	tempo.append(y)
 regeneracao = int(input())
 
@@ -14,7 +17,14 @@ vida -= dano_total
 for i in range(0,tempo[len(tempo)-1]):
 	if vida<100:
 		vida+=5
-if vida>0:
-	print("Inimigo Vivo")
-else:
+aux = 0
+for i in range(len(dano_total_lista)):
+	if dano_total_lista[i] >=100:
+		aux +=1
+if aux>=1:
 	print("Inimigo Morto")
+else:
+	if vida>0:
+		print("Inimigo Vivo")
+	else:
+		print("Inimigo Morto")
