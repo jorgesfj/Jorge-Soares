116c8f316015328552dc3ab136b59fe9480f66d4
Jorge <junior-soares42@hotmail.com>
2018-09-03 18:37:33 -0300

    b

--- Estruturas_de_Repetição_e_Dados/tradução.py
+++ Estruturas_de_Repetição_e_Dados/tradução.py
@@ -13,16 +13,17 @@ while n!= 0 and m!=0:
 		texto = input()
 		lista.append(texto)
 		cont+=1
+	lista2 = []
+	for i in range(len(lista)):
+		lista2.append(lista[i].split())
+	for i in range(len(lista2)):
+		for j in range(len(lista2[i])):
+			for w in range(0,len(substituicao),2):
+				if substituicao[w] == lista2[i][j]:
+					lista2[i][j] = substituicao[w+1]
+	for i in range(len(lista2)):
+		for j in range(len(lista2[i])):
+			print(lista2[i][j], end =" ")
+		print("\n")
+	substituicao = []
 	n,m = list(map(int,input().split()))
-lista2 = []
-for i in range(len(lista)):
-	lista2.append(lista[i].split())
-for i in range(len(lista2)):
-	for j in range(len(lista2[i])):
-		for w in range(0,len(substituicao),2):
-			if substituicao[w] == lista2[i][j]:
-				lista2[i][j] = substituicao[w+1]
-for i in range(len(lista2)):
-	for j in range(len(lista2[i])):
-		print(lista2[i][j], end =" ")
-	print("\n")
