b6792d81a576e0fdf71619f8722333e11704a9a1
Jorge <junior-soares42@hotmail.com>
2018-11-17 18:22:45 -0200

    inicioo

--- Atividade-Condicionais_e_Estruturas_de_Repetição/caracteres.py
+++
@@ -1,8 +0,0 @@
-a = int(input())
-s = input()
-print(s[::-1])
-while a != 0:
-	a = int(input())
-	if a>0: 
-		s = input()
-		print(s[::-1])
--- TesteDeConhecimentoInicial/triangular.py
+++
@@ -1,9 +0,0 @@
-n = int(input())
-cont = 1
-while cont * (cont+1) * (cont+2) < n:
-    cont += 1
-if cont * (cont+1) * (cont+2) == n:
-    print('''Verdadeiro
-{}*{}*{} = {}''' .format(cont,cont+1,cont+2,n))
-else:
-    print("Falso")
