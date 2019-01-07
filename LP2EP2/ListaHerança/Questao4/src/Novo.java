beeed67a3713824722a2efc4b791170d10d1946b
Jorge <junior-soares42@hotmail.com>
2019-01-07 16:52:31 -0200

    'add

---
+++ LP2EP2/ListaHerança/Questao4/src/Novo.java
@@ -0,0 +1,12 @@
+public class Novo extends Imovel{
+	private double adicional;
+
+	public double getAdicional() {
+		return adicional;
+	}
+
+	public void setAdicional(double adicional) {
+		this.adicional = adicional;
+	}
+
+}
