c80080a771a2013039944268e2b91bc60c63b31f
Jorge <junior-soares42@hotmail.com>
2019-01-07 14:57:09 -0200

    add

---
+++ LP2EP2/ListaHerança/Questao1/src/Ingresso.java
@@ -0,0 +1,20 @@
+public class Ingresso {
+
+	private float valor;
+	
+	public String toString(float valor) {
+		return ""+this.valor;
+	}
+
+	
+	
+	public float getValor() {
+		return valor;
+	}
+
+	public void setValor(float valor) {
+		this.valor = valor;
+	}
+	
+	
+}
